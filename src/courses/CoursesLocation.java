185054f137087fad187cb635147432ab7d3443ad
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2021-04-19 11:52:41 -0400

    Update CoursesLocation.java

--- src/courses/CoursesLocation.java
+++ src/courses/CoursesLocation.java
@@ -8,7 +8,7 @@ package courses;
 /** A class that provides the location where courses are offered.
  * We only ever want one location to provide 
  * course database and Softwaredesign to ensure consistency.
- * @author Sivagama Srinivasan
+ * @author Sivagama Srinivasan April 19
  */
 public class CoursesLocation {
    public int numofstudents = 0;// how many courses to add in semester
